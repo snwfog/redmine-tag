3e60456dfb31c9dacf8059fb92f9096a04cb78a7
Charles Yang <cchaoyang@8d.com>
2013-08-06 13:26:06 -0400

    Add to_s for tag, default scope and show tags string in update form

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,19 @@
 class Tag < ActiveRecord::Base
   unloadable
+
+  default_scope { includes(:tag_descriptor) }
+
   belongs_to :issue
   belongs_to :tag_descriptor
 
-  validates :severity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9 }
 
+  validates :severity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9 }
 
   validates :tag_descriptor_id, uniqueness: { scope: [:issue_id, :severity] }
 
-  scope :tags, -> { includes(:tag_descriptor) }
+
+  def to_s
+    self.tag_descriptor.description
+  end
 end
 
